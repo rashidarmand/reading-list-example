b7e7ae9651eefaea1666d7d513d8438b08b14b38
Rashid H Armand <rashidharmand@gmail.com>
2019-02-02 11:59:05 -0500

    added author and book models

--- server/app.js
+++ server/app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// Connect to MongoDB Atlas db cluster
+mongoose.connect('mongodb+srv://rashid-armand:Brooklyn_718@fcc-graphql-amwpq.mongodb.net/fcc-graphql?retryWrites=true', { useNewUrlParser: true });
+mongoose.connection.once('open', () => {
+  console.log('Connected to database.');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
